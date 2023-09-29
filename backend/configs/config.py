1e2e95240b6c0e95679dc25deafd22e485952b81
julianooii <julianooi80@gmail.com>
2023-09-29 17:58:24 +0800

    SQL Alchemy Configs

---
+++ backend/configs/config.py
@@ -0,0 +1,10 @@
+import os
+from os import environ
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/spm'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle': 299}
