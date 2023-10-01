1eabd215bbe8dadac4d363f584c0f441c2470414
aldensmu <aldenhjz@gmail.com>
2023-10-01 17:54:04 +0800

    created role listing page

--- spmfrontend/src/components/useAuth.jsx
+++
@@ -1,8 +0,0 @@
-// Function to check if the user is authenticated based on localStorage
-export function useAuth() {
-
-    const userToken = localStorage.getItem('userToken');
-  
-    // Return true if key 'userToken' is set, thus user is authenticated, false otherwise
-    return !!userToken;
-}
