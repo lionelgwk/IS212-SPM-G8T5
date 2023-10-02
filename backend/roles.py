eff06dfd8c76d8252d3b83c61c75812b0e9a9568
julianooii <julianooi80@gmail.com>
2023-10-02 13:08:15 +0800

    Cleaned up comments

--- backend/roles.py
+++ backend/roles.py
@@ -7,11 +7,6 @@ from datetime import date, timedelta, datetime
 from models import RoleListings, RoleDetails
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') or 'mysql+mysqlconnector://root@localhost:3306/spm'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
-
-# db = SQLAlchemy(app)
 
 CORS(app)
 
@@ -40,6 +35,30 @@ def getAllOpenRoles():
     current_date = datetime.now().date()
     all_role_listings = RoleListings.query.filter(RoleListings.role_listing_close > current_date).all()
     role_details_json_list = [listing.json() for listing in all_role_listings]
+
+    for role_details_json in role_details_json_list:
+        role_id = role_details_json["role_id"]
+        role = RoleDetails.query.get(role_id)
+        print("\n#######################")
+        print(role_id)
+        print(role.json())
+        role_details_json["role_name"] = role.json()["role_name"]
+        print(role_details_json)
+    
+    return jsonify(
+        {
+            "code" : 200,
+            "message" : "GET request successful",
+            "data" : role_details_json_list
+        }
+    )
+
+
+@role_bp.route('/new_role_listing', methods=["POST"])
+def getAllOpenRoles():
+    current_date = datetime.now().date()
+    all_role_listings = RoleListings.query.filter(RoleListings.role_listing_close > current_date).all()
+    role_details_ json_list = [listing.json() for listing in all_role_listings]
 
     for role_details_json in role_details_json_list:
         role_id = role_details_json["role_id"]
