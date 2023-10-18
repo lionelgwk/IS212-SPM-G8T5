e5d25ea5341aafeb47162dd13cf022d7e4460f15
yimengchai12 <yimeng.chai.2021@scis.smu.edu.sg>
2023-10-18 15:13:00 +0800

    added background color

--- spmfrontend/src/pages/ApplicantsPage.jsx
+++ spmfrontend/src/pages/ApplicantsPage.jsx
@@ -11,7 +11,7 @@ const ApplicantsPage = () => {
 
     return (
         <div className="bg-[#bee9e8] ">
-            <div className="container mx-auto p-2">
+            <div className="container mx-auto p-2 min-h-screen">
 
                 <div className="font-bold text-xl pt-5 mb-1 text-center">Applicants for Listing {id}</div>
                 <ApplicantSearch />
