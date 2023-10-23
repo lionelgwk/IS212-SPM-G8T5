d766e46c0fb1b0852b0bafa47ddc0f68c4bd8745
lionelgwk <lionelgwk@icloud.com>
2023-10-23 23:00:19 +0800

    minor improvements

--- spmfrontend/src/components/ApplyModal.jsx
+++ spmfrontend/src/components/ApplyModal.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ApplyModal = (props) => {
+  const [toastVisible, setToastVisible] = useState(false);
+  const [animateOut, setAnimateOut] = useState(false);
+
+  const handleApply = () => {
+    console.log("submitted");
+    setToastVisible(true);
+    setAnimateOut(false); // Make sure it's set to false when showing the toast
+
+    setTimeout(() => {
+      setAnimateOut(true); // Start the animation to slide out after 3 seconds
+
+      setTimeout(() => {
+        setToastVisible(false); // Hide the toast after the animation completes
+      }, 300); // The duration of the animation
+    }, 3000);
+  };
+
   return (
     <>
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
       <button
-        className="mx-2 px-4 py-2 bg-[#62b6cb] text-white hover:bg-[#1b4965] rounded-full shadow-sm"
+        className="mx-2 px-4 py-2 bg-[#62b6cb] text-white hover:bg-[#1b4965] rounded-full shadow-sm disabled:opacity-75 disabled:bg-[#62b6cb] disabled:cursor-not-allowed"
         onClick={() =>
           document.getElementById(`my_modal_${props.id}`).showModal()
         }
+        // disabled
       >
         Apply
       </button>
@@ -26,8 +44,14 @@ const ApplyModal = (props) => {
           </p>
           <p className="mb-5">Are you sure you want to apply for this role?</p>
           <div className="flex flex-row justify-center items-center">
-            <form method="dialog" className="flex flex-row justify-center items-center">
-              <button className="mx-4 px-4 py-2 bg-success rounded-xl shadow-sm flex items-center w-24 justify-center">
+            <form
+              method="dialog"
+              className="flex flex-row justify-center items-center"
+            >
+              <button
+                onClick={handleApply}
+                className="mx-4 px-4 py-2 bg-success rounded-xl shadow-sm flex items-center w-24 justify-center"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -64,6 +88,17 @@ const ApplyModal = (props) => {
           </div>
         </div>
       </dialog>
+      {toastVisible && (
+        <div className="toast toast-end">
+          <div
+            className={`alert alert-success transition-all duration-300 ${
+              animateOut ? "transform translate-x-full" : ""
+            }`}
+          >
+            <span>Message sent successfully.</span>
+          </div>
+        </div>
+      )}
     </>
   );
 };
--- spmfrontend/src/components/StaffNavBar.jsx
+++ spmfrontend/src/components/StaffNavBar.jsx
@@ -13,7 +13,7 @@ const StaffNavBar = () => {
       <header className="border-b-[1px] bg-[#e7f2f8] sticky top-0 z-50 shadow-md">
         <div className="container ml-4 text-navbar-primary top-0 h-[64px] items-center flex justify-between">
           <div className="flex gap-5 items-center justify-center">
-            <Link to="/" className="mr-2">
+            <Link to="/staff" className="mr-2">
               <img src="/src/assets/logo.png" alt="logo" className="h-[30px]" />
             </Link>
             <NavLink
