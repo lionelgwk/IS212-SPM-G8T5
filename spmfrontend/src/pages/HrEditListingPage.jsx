084d2d494afac5986443ddf7c39f4aca42959d9d
lionelgwk <lionelgwk@icloud.com>
2023-10-25 20:01:25 +0800

    converted 'My Listings' into 'Edit Listing'

---
+++ spmfrontend/src/pages/HrEditListingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import HrRoleCard from '../components/hrRoleCard'
+import FetchListingDetails from '../hook/FetchListingDetails'
+import { useParams } from 'react-router-dom'
+
+const HrEditListingPage = () => {
+  const { id } = useParams();
+  const { data: listing, isPending: isPending } = FetchListingDetails(id);
+  
+  return (
+    <div className="bg-[#bee9e8] h-screen">
+      <div className="container mx-auto p-2">
+        <div className="font-bold text-xl pt-10 mb-4 text-center">Listings</div>
+        <HrRoleCard id={101} />
+      </div>
+  </div>
+  )
+}
+
+export default HrEditListingPage
